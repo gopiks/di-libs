92ae047651779ab9e0b9e6093b16572ca150e1cb
Gopi Krishna Suvanam <apple@Gopis-MacBook-Air.local>
2023-01-24 14:01:41 +0530

    export

--- src/math/norm_dist.js
+++ src/math/norm_dist.js
@@ -1,4 +1,4 @@
-function normalcdf(X){   //HASTINGS.  MAX ERROR = .000001
+export function normalcdf(X){   //HASTINGS.  MAX ERROR = .000001
 	var T=1/(1+.2316419*Math.abs(X));
 	var D=.3989423*Math.exp(-X*X/2);
 	var Prob=D*T*(.3193815+T*(-.3565638+T*(1.781478+T*(-1.821256+T*1.330274))));
@@ -43,4 +43,3 @@ nrand = function() {
 	return x1 * c;
 }
 
-export normalcdf;
